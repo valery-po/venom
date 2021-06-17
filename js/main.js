da0a766a6c987fd3bed8ac98a4ec7715ec7ce90e
valery-po <54510559+valery-po@users.noreply.github.com>
2021-06-18 00:05:42 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const menuButton =  document.querySelector(".menu-button");
+const menu = document.querySelector(".nav-menu");
+const menuCloseButton =  document.querySelector(".menu-close");
+
+
+menuButton.addEventListener("click", () => {
+  menu.classList.add("is-active");
+  menuCloseButton.classList.toggle("is-active");
+});
+
+menuCloseButton.addEventListener("click", () => {
+  menu.classList.remove("is-active");
+  menuCloseButton.classList.remove("is-active");
+})
